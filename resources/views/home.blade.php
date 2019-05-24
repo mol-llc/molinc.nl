2ed31d7faf253f4e8e415f78d5b1258055bdb341
Gozertje <31706719+Gozertje@users.noreply.github.com>
2019-05-24 17:57:57 +0200

    + home pagina // er zijn wat dingen aan de home pagina toegevoed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <div id="wrap">
+        <div id="home-container">
+            <div class="row">
+                <div class="col">
+                    <h1>Mol</h1>
+                    <button type="button" class="button mx-auto">Services</button>
                 </div>
+                <div id="image" class="col">hksdhdshfjhfjsdhfjdsf</div>
             </div>
         </div>
+        <div class="container">
+            ha
+        </div>
     </div>
-</div>
 @endsection
